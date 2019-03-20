231d9e8cc04d5a5ea1bd446499ca114f7e66be8e
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro.local>
2019-03-20 14:35:30 -0600

    evenFinder

--- practice.js
+++ practice.js
@@ -52,7 +52,7 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 function looper(family){
-  for (i = 0; i<family.length; i++){
+  for (let i = 0; i<family.length; i++){
     alert(family[i])
   }
 }
@@ -70,7 +70,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
+function reversedLooper(letters){
+  for (i = letters.length-1; i >=0; i--){
+    alert(letters[i])
+  }
+}
 
 
 
@@ -85,7 +89,18 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  let newArray= []
+  let j = 0
+  for (i = 0; i <nums.length;i++){
+    if ((nums[i] % 2) === 0){
+      newArray[j] = nums[i]
+      j++
+    }
+  }
+  return newArray
+
+}
 
 
 
